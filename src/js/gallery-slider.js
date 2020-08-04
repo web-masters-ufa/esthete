cfb95158b87fa3c818f76444d0140b32cb93cc6a
UwetU <grafcf@gmail.com>
2020-08-04 21:54:26 +0500

    feat: gallery fix

--- src/js/gallery-slider.js
+++ src/js/gallery-slider.js
@@ -1,36 +1,40 @@
 import Swiper, { Pagination, Navigation } from 'swiper'
 
-Swiper.use([Pagination, Navigation])
+Swiper.use([Pagination, Navigation]);
 
 const portfolioImages = document.getElementsByClassName('portfolio__item');
-const galleryModal = document.getElementById('gallery');
-const closeModalGallery = document.getElementById('close-modal-gallery');
+const closeModalGallery = document.getElementsByClassName('modal-gallery__close');
 const body = document.getElementById('body');
 
-const swiper1 = new Swiper(`.modal-gallery__slider`, {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: `.swiper-pagination`,
-    clickable: true,
-  },
-})
-
 for (let i = 0; i < portfolioImages.length; i++) {
+  let galleryID = `gallery${i+1}`;
+  let galleryModal = document.getElementById(`${galleryID}`);
+
+  new Swiper( `.modal-gallery__slider${i+1}`, {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    observer: true,
+    observeParents: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: `.swiper-pagination`,
+      clickable: true,
+    },
+  });
+
   portfolioImages[i].addEventListener('click', () => {
     body.classList.toggle('overflow');
     galleryModal.classList.toggle('modal-container--open');
   })
-}
 
-closeModalGallery.addEventListener('click', () => {
-  body.classList.toggle('overflow');
-  galleryModal.classList.toggle('modal-container--open');
-})
+  closeModalGallery[i].addEventListener('click', () => {
+    body.classList.toggle('overflow');
+    galleryModal.classList.toggle('modal-container--open');
+  })
+};
 
 
 
