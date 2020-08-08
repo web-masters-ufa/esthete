1e6da6b165fc6212666a82e03d8b036e138f8038
UwetU <grafcf@gmail.com>
2020-08-08 19:55:26 +0500

    mobile

--- src/js/gallery-slider.js
+++ src/js/gallery-slider.js
@@ -2,39 +2,54 @@ import Swiper, { Pagination, Navigation } from 'swiper'
 
 Swiper.use([Pagination, Navigation]);
 
-const portfolioImages = document.getElementsByClassName('portfolio__item');
-const closeModalGallery = document.getElementsByClassName('modal-gallery__close');
-const body = document.getElementById('body');
+const portfolioImages = document.querySelectorAll('.portfolio__item');
+const closeModalGallery = document.querySelectorAll('.modal-gallery__close');
+const body = document.querySelector('#body');
+let galleryModal = document.querySelectorAll('.modal-container-gallery');
 
-for (let i = 0; i < portfolioImages.length; i++) {
-  let galleryID = `gallery${i+1}`;
-  let galleryModal = document.getElementById(`${galleryID}`);
+(function () {
+  let modalSlider = document.querySelectorAll('.modal-gallery__slider')
 
-  new Swiper( `.modal-gallery__slider${i+1}`, {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    observer: true,
-    observeParents: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: `.swiper-pagination`,
-      clickable: true,
-    },
+  function sliderCollection (sliders) {
+    sliders.forEach(elem => {
+      new Swiper( elem, {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        observer: true,
+        observeParents: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: `.swiper-pagination`,
+          clickable: true,
+        },
+      });
+    })
+  }
+
+  sliderCollection(modalSlider);
+
+})();
+
+portfolioImages.forEach(item => {
+  item.addEventListener('click', () => {
+    body.classList.toggle('overflow');
+    galleryModal.forEach(item => {
+      item.classList.toggle('modal-container--open');
+    });
   });
+});
 
-  portfolioImages[i].addEventListener('click', () => {
+closeModalGallery.forEach(item => {
+  item.addEventListener('click', () => {
     body.classList.toggle('overflow');
-    galleryModal.classList.toggle('modal-container--open');
-  })
-
-  closeModalGallery[i].addEventListener('click', () => {
-    body.classList.toggle('overflow');
-    galleryModal.classList.toggle('modal-container--open');
-  })
-};
+    galleryModal.forEach(item => {
+      item.classList.toggle('modal-container--open');
+    });
+  });
+});
 
 
 
